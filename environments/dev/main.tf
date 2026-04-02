7b6a0cacd0b3cea8b51463a1104d8bf680f0d564
naraharivarshinireddy-max <naraharivarshinireddy@gmail.com>
2026-04-02 22:53:56 +0530

    feat: add remote backend (s3 + dynamodb)

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,1 +1,21 @@
-# trigger atlantis
+terraform {
+  backend "s3" {
+    bucket         = "atlantis-terraform-state-project"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "atlantis-terraform-locks"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  name          = var.name
+}
