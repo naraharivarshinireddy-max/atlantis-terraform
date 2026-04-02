e2103dd5e868d2f718bfe8391da2646f4b8b55ca
naraharivarshinireddy-max <naraharivarshinireddy@gmail.com>
2026-04-02 20:44:06 +0530

    refactor: proper terraform structure with modules and env

---
+++ environments/dev/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
