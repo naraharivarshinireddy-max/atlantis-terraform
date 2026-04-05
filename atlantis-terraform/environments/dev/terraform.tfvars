8b259a453435ff299cd2e12ccc2d70991687df41
naraharivarshinireddy-max <naraharivarshinireddy@gmail.com>
2026-04-06 03:29:45 +0530

    fix: remove nested repo and track files properly

---
+++ atlantis-terraform/environments/dev/terraform.tfvars
@@ -0,0 +1,4 @@
+region         = "ap-south-1"
+instance_type  = "t3.micro"
+name           = "atlantis-dev"
+ami = "ami-0522ab6e1ddcc7055"
---
+++ atlantis-terraform/environments/staging/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = var.region
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  name          = var.name
+}
